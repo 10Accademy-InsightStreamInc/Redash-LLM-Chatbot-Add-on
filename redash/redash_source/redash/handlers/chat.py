4fd2eb4a10cb5de2c0294029b35caa9d8ffa2873
dev-abuke <abubekershamil123@gmail.com>
2024-05-15 15:51:45 +0300

    feat: Add chatbot functionality and query generation

--- redash/redash_source/redash/handlers/chat.py
+++ redash/redash_source/redash/handlers/chat.py
@@ -16,7 +16,7 @@ from redash.handlers.base import (
 )
 import requests
 from sqlalchemy import create_engine, inspect
-app = Flask(__name__)
+# app = Flask(__name__)
 VARIABLE_KEY = os.environ.get("OPENAI_API_KEY")
 client = OpenAI(
   api_key=VARIABLE_KEY
@@ -57,12 +57,35 @@ class ChatResource(BaseResource):
             new_table_column_string = new_table_column_string + key + ' is: ' + value
 
     def ask_database(self, query):
-        """Function to query Postgres database with a provided SQL query."""
+        """
+        Function to query Postgres database with a provided SQL query.
+        
+        This function takes a SQL query as input and executes it against the Postgres database.
+        The function establishes a connection to the database using the `engine` object created earlier.
+        The `text` function is used to create a SQLAlchemy TextClause object, which is then used to execute the query.
+        The `fetchall` method is called on the result to retrieve all rows from the query result.
+        
+        Parameters:
+        - query (str): The SQL query to be executed.
+        
+        Returns:
+        - result (list): A list of tuples, where each tuple represents a row of the query result.
+        
+        Exception Handling:
+        - If any exception occurs during the execution of the query, the function catches the exception and returns a string indicating the error.
+        """
         try:
-            # Assuming 'db.connection' is your SQLAlchemy connection
-            result = self.engine.connect().execute(text(query)).fetchall()
+            # Establish a connection to the database using the engine object.
+            connection = self.engine.connect()
+            # Create a SQLAlchemy TextClause object from the query string.
+            # This object is then used to execute the query against the database.
+            result = connection.execute(text(query)).fetchall()
+            # Close the database connection.
+            connection.close()
+            # Return the query result.
             return result
         except Exception as e:
+            # If an exception occurs, catch it and return a string indicating the error.
             results = f"query failed with error: {e}"
         return results
 
@@ -75,30 +98,31 @@ class ChatResource(BaseResource):
             results = f"Error: function {message['tool_calls'][0]['function']['name']} does not exist"
         return results
     
-    @retry(wait=wait_random_exponential(multiplier=1, max=40), stop=stop_after_attempt(3))
+    # @retry(wait=wait_random_exponential(multiplier=1, max=40), stop=stop_after_attempt(3))
     def post(self):
         try:
             value = request.get_json()
             question = value.get('question')
             completion = client.chat.completions.create(
-            model="gpt-3.5-turbo",
-            messages = [
-                {"role": "system",
-                    "content": f'''You are a helpful assistant who generates SQL queries on the database and responds from the database schema provided only\
-                    SQL should be written using this table and column names from database schema: {self.new_table_column_string}\
-                    Table Names or Column Names Referenced anywhere in your SQL Query when writing SQL MUST ALWAYS be in double quotes EVEN IF THEY ARE A SINGLE WORD, \
-                    The Database you will be writting SQL queries for is: postgres \
-                    You are always to the point and ALWAYS generate the SQL query and no additional explanation or text\
-                    Generate only SQL syntax when prompted about any inquiries regarding SQL queries.\
-                    The query should be returned in plain text, not in JSON \
-                    Output should be a fully formed SQL query.\
-                    Please YOU MUST USE DOUBLE QUOTES like "" in FROM, SELECT, WHERE, GROUP BY, ORDER BY, LIMIT and  all other clauses and on \
-                    all function in SQL on column names such as SUM() inside MUST BE CALLED WITH COLUMNS IN DOUBLE QUOTES for example if the Column name to be summed is City then use SUM("City") and others builtin SQL functions ARE MUST TO ADD QUOTES OR DONT ANSWER. \
-                    Example Like this: 'SELECT "City name", SUM("Views by Date") AS total_views, SUM("Total Watch time (hours) by Citiy name") AS total_watch_time FROM "Cities" GROUP BY "City name"'
-                    Do not use any kind of formatting on your response query such as newlines or indentation finish everything in one line. \
-                    Don't complicate things. Generate only SQL syntax when prompted about any inquiries regarding SQL queries.'''},
-                {"role": "user", "content": f"{question}"},
-            ])
+                model="gpt-3.5-turbo",
+                messages = [
+                    {"role": "system",
+                        "content": f'''You are a helpful assistant who generates SQL queries on the database and responds from the database schema provided only\
+                        SQL should be written using this table and column names from database schema: {self.new_table_column_string}\
+                        Table Names or Column Names Referenced anywhere in your SQL Query when writing SQL MUST ALWAYS be in double quotes EVEN IF THEY ARE A SINGLE WORD, \
+                        The Database you will be writting SQL queries for is: postgres \
+                        You are always to the point and ALWAYS generate the SQL query and no additional explanation or text\
+                        Generate only SQL syntax when prompted about any inquiries regarding SQL queries.\
+                        The query should be returned in plain text, not in JSON \
+                        Output should be a fully formed SQL query.\
+                        Please YOU MUST USE DOUBLE QUOTES like "" in FROM, SELECT, WHERE, GROUP BY, ORDER BY, LIMIT and  all other clauses and on \
+                        all function in SQL on column names such as SUM() inside MUST BE CALLED WITH COLUMNS IN DOUBLE QUOTES for example if the Column name to be summed is City then use SUM("City") and others builtin SQL functions ARE MUST TO ADD QUOTES OR DONT ANSWER. \
+                        Example Like this: 'SELECT "City name", SUM("Views by Date") AS total_views, SUM("Total Watch time (hours) by Citiy name") AS total_watch_time FROM "Cities" GROUP BY "City name"'
+                        Do not use any kind of formatting on your response query such as newlines or indentation finish everything in one line. \
+                        Don't complicate things. Generate only SQL syntax when prompted about any inquiries regarding SQL queries.'''},
+                    {"role": "user", "content": f"{question}"},
+                ]
+            )
             # tools = [
             #     {"type": "function",
             #         "function": {
@@ -126,42 +150,46 @@ class ChatResource(BaseResource):
             # resp = self.chat_completion_request(messages, tools, tool_choice=None, model=model)
                     # "Give me all cities names with there total views and watch time"
             # )
-            message = completion["choices"][0]["message"]
+            # message = completion["choices"][0]["message"]
             # query = json.loads(message["tool_calls"][0]["function"]["arguments"])["query"]
             # answer = self.execute_function_call(message)
-            answer = message["content"]
+            # answer = message["content"]
             # if answer == None:
             #     answer = "Hello this me abuki not ai"
             # if message == None:
             #     message = "This is the first message"
-            response_data = {"answer": f"{answer}"}
+            # response_data = {"answer": f"{answer}"}
+            # return jsonify(response_data), 200
+            
+            answer = completion.choices[0].message.content
+            response_data = {"answer": answer}
             return jsonify(response_data), 200
         except Exception as error:
             print(error)
             return jsonify({"error": "An error occurred"}), 500
 
-    @staticmethod    
-    @retry(wait=wait_random_exponential(multiplier=1, max=40), stop=stop_after_attempt(3))
-    def chat_completion_request(messages, tools=None, tool_choice=None, model="gpt-3.5-turbo-0613"):
-        headers = {
-            "Content-Type": "application/json",
-            "Authorization": "Bearer ",
-        }
-        json_data = {"model": model, "messages": messages}
-        if tools is not None:
-            json_data.update({"tools": tools})
-        if tool_choice is not None:
-            json_data.update({"tool_choice": tool_choice})
-        try:
-            response = requests.post(
-                "https://api.openai.com/v1/chat/completions",
-                headers=headers,
-                json=json_data,
-            )
-            return response.json()
-        except Exception as e:
-            print("Unable to generate ChatCompletion response")
-            print(f"Exception: {e}")
-            return e
-if __name__ == "__main__":
-    app.run(debug=True)
+#     @staticmethod    
+#     @retry(wait=wait_random_exponential(multiplier=1, max=40), stop=stop_after_attempt(3))
+#     def chat_completion_request(messages, tools=None, tool_choice=None, model="gpt-3.5-turbo-0613"):
+#         headers = {
+#             "Content-Type": "application/json",
+#             "Authorization": "Bearer ",
+#         }
+#         json_data = {"model": model, "messages": messages}
+#         if tools is not None:
+#             json_data.update({"tools": tools})
+#         if tool_choice is not None:
+#             json_data.update({"tool_choice": tool_choice})
+#         try:
+#             response = requests.post(
+#                 "https://api.openai.com/v1/chat/completions",
+#                 headers=headers,
+#                 json=json_data,
+#             )
+#             return response.json()
+#         except Exception as e:
+#             print("Unable to generate ChatCompletion response")
+#             print(f"Exception: {e}")
+#             return e
+# # if __name__ == "__main__":
+#     app.run(debug=True)
